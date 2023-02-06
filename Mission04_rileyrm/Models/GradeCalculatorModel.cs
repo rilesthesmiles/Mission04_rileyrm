d3fb66dfc05a505cbcd4ac3b84669bf64019e9fc
Riley Marshall <rileyrm@byu.edu>
2023-02-06 12:51:17 -0700

    Added some comments

--- Mission04_rileyrm/Models/GradeCalculatorModel.cs
+++ Mission04_rileyrm/Models/GradeCalculatorModel.cs
@@ -8,6 +8,9 @@ namespace Mission04_rileyrm.Models
 {
     public class GradeCalculatorModel
     {
+
+        // These are all the fields with the assignments for the grade calculator
+        // they are all required and need to be in the range of 0 - 100
         [Required]
         [Range(0,100)]
         public int Assignments { get; set; }
